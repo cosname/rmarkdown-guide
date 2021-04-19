5429d5ad233c0ce5e0d3ac1e40104a5a7b840eea
qiushi <qiushi.yann@gmail.com>
2021-04-19 11:04:21 +0800

    add deploy script

--- _deploy.sh
+++ _deploy.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${BRANCH}" != "master" ] && exit 0
 
 git config --global user.email "xie@yihui.name"
 git config --global user.name "Yihui Xie"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+git clone -b gh-pages https://github.com/${REPO}.git book-output
 cd book-output
 
 ls | xargs rm -rf
