06417fe3083331cfae6a939f25dc66241dd99d6b
Yihui Xie <xie@yihui.name>
2021-04-20 22:52:04 -0500

    不要用我的个人账号信息，令牌并非关联在我的账号上

--- _deploy.sh
+++ _deploy.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
-
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${REPO}.git book-output
 cd book-output
 
