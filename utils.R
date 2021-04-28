e0c42ebf6b349e3ad3981c07f0072d4a5ba8290a
liangliangzhuang <62544020+liangliangzhuang@users.noreply.github.com>
2021-04-28 17:58:02 +0800

    13- 16 chapter
     first commit

--- utils.R
+++ utils.R
@@ -14,3 +14,14 @@ import_example_result <- function(file, redo = FALSE, vwidth = 700, vheight = 40
   }
   knitr::include_graphics(webshot)
 }
+
+
+import_example <- function(file, lang = xfun::file_ext(file)) {
+  x = xfun::read_utf8(file.path("examples", file))
+  lang = tolower(lang)
+  if (nchar(lang) > 1) {
+    lang = sub('^r', '', lang)
+    if (lang == 'nw') lang = 'tex'
+  }
+  knitr::asis_output(paste(c(sprintf("````%s", lang), x, "````"), collapse = '\n'))
+}
