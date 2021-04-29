1a595738befd16c1d9c7aa3de8583bb01298f9ad
Chun-Hui Gao <gaospecial@gmail.com>
2021-04-29 11:02:23 +0800

    render examples from a new R session

--- utils.R
+++ utils.R
@@ -2,15 +2,17 @@ import_example_result <- function(file, redo = FALSE, vwidth = 700, vheight = 40
   file <- file.path("examples",file)
   webshot <- paste0(xfun::with_ext(file, "png"))
   if (xfun::file_exists(webshot)){
-    webshot_time <- file.info(webshot)$mtime
-    source_time <- file.info(file)$mtime
-    if (source_time > webshot_time) redo <- TRUE
+    fail <- tryCatch(png::readPNG(file), error = function(x) TRUE)
+    outdate <- file.info(file)$mtime > file.info(webshot)$mtime
+    if (fail | outdate) redo <- TRUE
   } else {
     redo <- TRUE
   }
   if (redo){
-    output <- rmarkdown::render(file, quiet = TRUE)
-    webshot::webshot(url = output, file = webshot, vwidth = vwidth, vheight = vheight, ...)
+    cmd <- sprintf("rmarkdown::render('%s', quiet = TRUE)", file)
+    ret <- xfun::Rscript(c("-e", shQuote(cmd)))
+    if (ret == 0) url <- xfun::with_ext(file,"html")
+    webshot::webshot(url = url, file = webshot, vwidth = vwidth, vheight = vheight, ...)
   }
   knitr::include_graphics(webshot)
 }
