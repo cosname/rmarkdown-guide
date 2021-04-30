ec5ea215c4b08fd5178e3f2d967e026d2aac8969
Chun-Hui Gao <gaospecial@gmail.com>
2021-04-30 08:50:25 +0800

    webshot::rmdshot is enough

--- utils.R
+++ utils.R
@@ -1,4 +1,5 @@
 import_example_result <- function(file, redo = FALSE, vwidth = 700, vheight = 400, ...){
+  file <- xfun::magic_path(file)
   webshot <- paste0(xfun::with_ext(file, "png"))
   if (xfun::file_exists(webshot)){
     fail <- tryCatch(png::readPNG(file), error = function(x) TRUE)
@@ -8,10 +9,7 @@ import_example_result <- function(file, redo = FALSE, vwidth = 700, vheight = 40
     redo <- TRUE
   }
   if (redo){
-    cmd <- sprintf("rmarkdown::render('%s', quiet = TRUE)", file)
-    ret <- xfun::Rscript(c("-e", shQuote(cmd)))
-    if (ret == 0) url <- xfun::with_ext(file,"html")
-    webshot::webshot(url = url, file = webshot, vwidth = vwidth, vheight = vheight, ...)
+    webshot::rmdshot(file, webshot, vwidth = vwidth, vheight = vheight, ...)
   }
   knitr::include_graphics(webshot)
 }
