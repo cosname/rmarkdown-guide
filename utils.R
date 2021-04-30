00b430cb2751804b3e51efec096a7459622153ae
Chun-Hui Gao <gaospecial@gmail.com>
2021-04-30 12:03:14 +0800

    automatic R Markdown screenshot

--- utils.R
+++ utils.R
@@ -1,6 +1,6 @@
 import_example_result <- function(file, redo = FALSE, vwidth = 700, vheight = 400, ...){
   file <- xfun::magic_path(file)
-  out <- names(yaml::read_yaml(file)$output)
+  out <- rmarkdown::all_output_formats(file)[[1]]
   webshot <- paste0(xfun::with_ext(file, "png"))
   if (xfun::file_exists(webshot)){
     fail <- tryCatch(png::readPNG(file), error = function(x) TRUE)
@@ -10,10 +10,17 @@ import_example_result <- function(file, redo = FALSE, vwidth = 700, vheight = 40
     redo <- TRUE
   }
   if (redo){
-    if (out %in% c("pdf_document","beamer_presentation"))
-      pdf = xfun::with_ext(file, "pdf")
-      content = magick::image_read_pdf(pdf)
-      magick::image_write(content, webshot)
+    if (out %in% c("pdf_document","beamer_presentation")){
+      cmd <- sprintf("rmarkdown::render('%s', quiet = TRUE)", file)
+      ret <- xfun::Rscript(c("-e", shQuote(cmd)))
+      if (ret == 0){
+        pdf = xfun::with_ext(file, "pdf")
+        content = magick::image_read_pdf(pdf)
+        magick::image_write(content, webshot)
+      } else {
+        stop(simpleError("Knit to PDF failed for document: ", file))
+      }
+    }
 
     if (out %in% c("html_document","ioslides_presentation","slidy_presentation"))
       webshot::rmdshot(file, webshot, vwidth = vwidth, vheight = vheight, ...)
