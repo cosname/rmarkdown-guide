549ac66b298b6cdb6d4c0871bf1a161c9dadeae0
Chun-Hui Gao <gaospecial@gmail.com>
2021-05-06 13:59:39 +0800

    新的章节：PDF 文档和 R Markdown 演示文稿 (#28)
    * skeleton
    
    * 开始写作
    
    * html-document
    
    * update
    
    * HTML dependency
    
    * update HTML document and utils
    
    - rmarkdown examples
    
    * update HTML document and examples
    
    * nice subheadings
    
    * add examples/html-fragment.html
    
    * update redirect URL
    
    * chapter PDF document
    
    * render examples from a new R session
    
    * use real path
    
    * webshot::rmdshot is enough
    
    * use magic_path() in import_example()
    
    * enable screenshot to PDF document
    
    * update chapter 7: PDF document
    
    * automatic R Markdown screenshot
    
    * one R Markdown, many output screenshot
    
    * Chapter 8. presentation
    
    * examples and screenshots
    
    * fixing minor errors
    
    * update env
    
    * import_example() already defined in index.Rmd
    
    * minor fixing
    
    * suppress png error
    
    * debug import_example_result()
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug

--- utils.R
+++ utils.R
@@ -1,27 +1,64 @@
 import_example_result <- function(file, redo = FALSE, vwidth = 700, vheight = 400, ...){
-  file <- file.path("examples",file)
-  webshot <- paste0(xfun::with_ext(file, "png"))
-  if (xfun::file_exists(webshot)){
-    webshot_time <- file.info(webshot)$mtime
-    source_time <- file.info(file)$mtime
-    if (source_time > webshot_time) redo <- TRUE
+  file <- xfun::magic_path(file)
+  out <- rmarkdown::all_output_formats(file)
+  if (is.null(out)) out = "html_document"
+  if (length(out) == 1){
+    webshot <- xfun::with_ext(file, "png")
+  }
+  if (length(out) > 1){
+    webshot = xfun::with_ext(file, paste0(out,".png"))
+  }
+
+  if (all(xfun::file_exists(webshot))){ # if all output is exist
+    fail <- tryCatch(png::readPNG(file), error = function(x) TRUE)
+    outdate <- file.info(file)$mtime > file.info(webshot)$mtime
+    if (fail | outdate) redo <- TRUE
   } else {
     redo <- TRUE
   }
   if (redo){
-    output <- rmarkdown::render(file, quiet = TRUE)
-    webshot::webshot(url = output, file = webshot, vwidth = vwidth, vheight = vheight, ...)
+    for (i in 1:length(out)){
+      fmt = out[[i]]
+      outfile = webshot[[i]]
+
+      if (fmt %in% c("pdf_document","beamer_presentation"))
+        tryCatch(rmd_pdf_screenshot(file, fmt, outfile),
+                 error = function(e)paste0("Failed to process ", file))
+
+      if (fmt %in% c("html_document","ioslides_presentation","slidy_presentation"))
+        tryCatch(rmd_html_screenshot(file, fmt, outfile, vwidth=vwidth, vheight=vheight),
+                 error = function(e)paste0("Failed to process ", file))
+
+    }
   }
-  knitr::include_graphics(webshot)
+  tryCatch(knitr::include_graphics(webshot), error = function(e) paste0("unable to load png: ", webshot))
 }
 
+rmd_html_screenshot <- function(file, fmt = "html_document", outfile = xfun::with_ext(file, "png"), ...){
+  cmd <- sprintf("rmarkdown::render('%s', '%s', quiet = TRUE)", file, fmt)
+  ret <- xfun::Rscript(c("-e", shQuote(cmd)))
+  if (ret != 0) stop(simpleError("Knit to HTML failed for document: ", file))
+  url = xfun::with_ext(file, "html")
+  html_screenshot(url, outfile, ...)
+}
 
-import_example <- function(file, lang = xfun::file_ext(file)) {
-  x = xfun::read_utf8(file.path("examples", file))
-  lang = tolower(lang)
-  if (nchar(lang) > 1) {
-    lang = sub('^r', '', lang)
-    if (lang == 'nw') lang = 'tex'
-  }
-  knitr::asis_output(paste(c(sprintf("````%s", lang), x, "````"), collapse = '\n'))
+html_screenshot <- function(url, outfile = xfun::with_ext(url,"png"), ...){
+  webshot::webshot(url, outfile, ...)
+  return(outfile)
+}
+
+rmd_pdf_screenshot <- function(file, fmt = "pdf_document", outfile = xfun::with_ext(file, "png")){
+  cmd <- sprintf("rmarkdown::render('%s', '%s', quiet = TRUE)", file, fmt)
+  ret <- xfun::Rscript(c("-e", shQuote(cmd)))
+  if (ret != 0) stop(simpleError("Knit to PDF failed for document: ", file))
+
+  pdf = xfun::with_ext(file, "pdf")
+  pdf_screenshot(pdf, outfile)
+
+}
+
+pdf_screenshot <- function(pdf, outfile = xfun::with_ext(pdf, "png")){
+  content = magick::image_read_pdf(pdf, pages = 1)
+  magick::image_write(content, outfile)
+  return(outfile)
 }
